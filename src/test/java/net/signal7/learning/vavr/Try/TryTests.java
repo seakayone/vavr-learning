d9afa5a3ace0bf881003e541fdb72fc6290bed4a
Christian Kleinboelting <christian@kleinboelting.de>
2019-01-25 11:24:51 +0100

    Add TryTest

---
+++ src/test/java/net/signal7/learning/vavr/Try/TryTests.java
@@ -0,0 +1,28 @@
+package net.signal7.learning.vavr.Try;
+
+import static org.assertj.vavr.api.VavrAssertions.assertThat;
+
+import io.vavr.control.Try;
+import java.io.InputStream;
+import java.net.MalformedURLException;
+import java.net.URL;
+import lombok.val;
+import org.junit.jupiter.api.Test;
+
+class TryTests {
+
+  @Test
+  void create_an_url() throws MalformedURLException {
+    val url = Try.of(() -> new URL("http://signal7.de"));
+    assertThat(url).contains(new URL("http://signal7.de"));
+  }
+
+  @Test
+  void access_an_url() {
+    val in = Try.of(() -> new URL("http://signal7.de"))
+        .flatMap(url -> Try.of(url::openConnection))
+        .flatMap(con -> Try.of(con::getInputStream));
+    assertThat(in).containsInstanceOf(InputStream.class);
+  }
+
+}
