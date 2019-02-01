daed4fb57ba828047963cf155ef55fd76bc0fac7
Christian Kleinboelting <christian@kleinboelting.de>
2019-02-01 23:39:04 +0100

    Add buy coffee examples

---
+++ src/test/java/net/signal7/learning/vavr/examples/coffee/BuyCoffeeFunctional.java
@@ -0,0 +1,79 @@
+package net.signal7.learning.vavr.examples.coffee;
+
+import io.vavr.Tuple;
+import io.vavr.Tuple2;
+import io.vavr.collection.List;
+import lombok.NonNull;
+import lombok.Value;
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.Test;
+
+import static org.assertj.core.api.Assertions.assertThat;
+
+class BuyCoffeeFunctional {
+
+  class Cafe {
+    Tuple2<Coffee, Charge> buyCoffee(CreditCard cc) {
+      final var cup = new Coffee();
+      final var charge = new Charge(cc, cup.getPrice());
+      return Tuple.of(cup, charge);
+    }
+
+    Tuple2<List<Coffee>, Charge> buyCoffees(CreditCard cc, int n) {
+      var purchases = List.fill(n, () -> buyCoffee(cc));
+      return purchases.unzip(e -> Tuple.of(e._1, e._2))
+        .map2(charges -> charges.reduce(Charge::combine));
+    }
+  }
+
+  @Value
+  class Coffee {
+    long price = 250;
+  }
+
+  @Value
+  class CreditCard {
+    @NonNull String number;
+  }
+
+  @Value
+  class Charge {
+    @NonNull CreditCard cc;
+    long amount;
+
+    Charge combine(Charge other) {
+      if (this.cc == other.cc) {
+        return new Charge(cc, this.amount + other.amount);
+      } else {
+        throw new IllegalArgumentException("Cannot combine charges from different credit cards");
+      }
+    }
+  }
+
+  @BeforeEach
+  void setup() {
+    this.cafe = new Cafe();
+    this.cc = new CreditCard("nr");
+  }
+
+  private Cafe cafe;
+  private CreditCard cc;
+
+  @Test
+  void buy_a_coffees() {
+    final var bought = cafe.buyCoffee(cc);
+
+    assertThat(bought._1).isNotNull();
+    assertThat(bought._2.getAmount()).isEqualTo(250L);
+    assertThat(bought._2.getCc()).isEqualTo(cc);
+  }
+
+  @Test
+  void buy_2_coffees() {
+    final var bought = cafe.buyCoffees(cc, 2);
+
+    assertThat(bought._1).hasSize(2);
+    assertThat(bought._2.getAmount()).isEqualTo(500L);
+    assertThat(bought._2.getCc()).isEqualTo(cc);
+  }
+}
---
+++ src/test/java/net/signal7/learning/vavr/examples/coffee/BuyCoffeeImperative.java
@@ -0,0 +1,73 @@
+package net.signal7.learning.vavr.examples.coffee;
+
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.Value;
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.Test;
+
+import java.util.ArrayList;
+import java.util.List;
+
+import static org.assertj.core.api.Assertions.assertThat;
+
+class BuyCoffeeImperative {
+  
+  class Cafe {
+    Coffee buyCoffee(CreditCard cc) {
+      var cup = new Coffee();
+      cc.charge(cup.getPrice());
+      return cup;
+    }
+
+    List<Coffee> buyCoffees(CreditCard cc, int n) {
+      var coffees = new ArrayList<Coffee>();
+      while (n > 0) {
+        var cup = buyCoffee(cc);
+        coffees.add(cup);
+        n--;
+      }
+      return coffees;
+    }
+  }
+
+  @Value
+  class Coffee {
+    long price = 250;
+  }
+
+  @Data
+  @AllArgsConstructor
+  class CreditCard {
+    private long balance;
+
+    void charge(long amount) {
+      balance = balance - amount;
+    }
+  }
+
+  @BeforeEach
+  void setup() {
+    this.cafe = new Cafe();
+    this.cc = new CreditCard(500);
+  }
+
+  private Cafe cafe;
+  private CreditCard cc;
+
+  @Test
+  void buy_a_coffee() {
+    var cup = cafe.buyCoffee(cc);
+
+    assertThat(cup).isNotNull();
+    assertThat(cc.getBalance()).isEqualTo(250);
+  }
+
+  @Test
+  void buy_2_coffees() {
+    var cup = cafe.buyCoffees(cc, 2);
+
+    assertThat(cup).hasSize(2);
+    assertThat(cc.getBalance()).isEqualTo(0);
+  }
+}
