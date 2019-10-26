fb1ddbd35e0f5c83115bc35eeee82517d4e710b2
Christian Kleinboelting <kleinboelting@signal7.de>
2019-10-26 13:35:00 +0200

    Add Either tests

---
+++ src/test/java/net/signal7/learning/vavr/value/either/EitherTests.java
@@ -0,0 +1,47 @@
+package net.signal7.learning.vavr.value.either;
+
+import static org.assertj.core.api.Assertions.assertThat;
+import static org.assertj.core.api.Assertions.assertThatThrownBy;
+
+import io.vavr.control.Either;
+import java.util.function.Function;
+import lombok.Value;
+import lombok.val;
+import org.junit.jupiter.api.Test;
+
+class EitherTests {
+
+  @Value(staticConstructor = "of")
+  static class Failed {
+
+    String reason;
+  }
+
+  @Test
+  void failed_with_fatal_reason_should_throw() {
+    val failure = Either.<Failed, String>left(Failed.of("fatal"));
+    assertThatThrownBy(() -> getSuccessOrThrow(failure))
+        .isInstanceOf(IllegalArgumentException.class);
+  }
+
+  private String getSuccessOrThrow(Either<Failed, String> failure) {
+    return failure.swap().flatMap(this::successOnNonFatal).swap()
+        .mapLeft(Failed::getReason)
+        .getOrElseThrow((Function<String, IllegalArgumentException>) IllegalArgumentException::new);
+  }
+
+  private Either<String, Failed> successOnNonFatal(Failed failed) {
+    final String reason = failed.getReason();
+    if ("fatal".equals(reason)) {
+      return Either.right(failed);
+    }
+    return Either.left(failed.getReason());
+  }
+
+  @Test
+  void failed_with_nonfatal_reason_should_throw() {
+    Either<Failed, String> failure = Either.left(Failed.of("nonfatal"));
+
+    assertThat(getSuccessOrThrow(failure)).isEqualTo("nonfatal");
+  }
+}
