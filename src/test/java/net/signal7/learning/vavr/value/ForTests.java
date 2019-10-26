ee2e3017a91644b1ec192c0cdb20def85e9981b2
Christian Kleinboelting <kleinboelting@signal7.de>
2019-10-26 20:57:15 +0200

    Add for comprehension

---
+++ src/test/java/net/signal7/learning/vavr/value/ForTests.java
@@ -0,0 +1,42 @@
+package net.signal7.learning.vavr.value;
+
+import static io.vavr.API.CharSeq;
+import static io.vavr.API.For;
+import static org.assertj.vavr.api.VavrAssertions.assertThat;
+
+import io.vavr.collection.Iterator;
+import io.vavr.collection.Stream;
+import io.vavr.control.Option;
+import org.junit.jupiter.api.Test;
+
+class ForTests {
+
+  @Test
+  void test1() {
+    assertThat(firstIndexOfAny("aba", new char[]{'z'}))
+        .isEmpty();
+  }
+
+  @Test
+  void test2() {
+    assertThat(firstIndexOfAny("zzabyycdxx", new char[]{'z', 'a'}))
+        .contains(0);
+  }
+
+  @Test
+  void test3() {
+    assertThat(firstIndexOfAny("zzabyycdxx", new char[]{'c', 'v', '0'}))
+        .contains(6);
+  }
+
+  static Option<Integer> firstIndexOfAny(String input, char[] searchChars) {
+    final Iterator<Option<Integer>> result = For(
+        CharSeq(input).zipWithIndex().toStream(),
+        Stream.ofAll(searchChars)
+    ).yield((pair, character) ->
+        pair._1 == character
+            ? Option.of(pair._2)
+            : Option.none());
+    return result.flatMap(Option::iterator).headOption();
+  }
+}
